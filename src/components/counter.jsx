938e9da6373f0099c5e0b5a2f12dd16b2fe5dadd
Parisa Pahlevan <paris.pahlevan@gmail.com>
2019-08-13 15:53:11 -0400

    added the Reset and Delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value: this.props.value
-       
-    };
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button 
+        onClick={() => this.props.onDelete(this.props.id)} 
+        className="btn.btn-danger btn-sm m-2"
+        >
+          Delete
+       </button>
+      </div> 
+    );
+  }
 
-    
-    handleIncrement = () => {
-        this.setState({value: this.state.value + 1});
-    }
-    
-    render() { 
-        return (
-        <React.Fragment>
-            <span  className= {this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} 
-            className= "btn btn-secondary btn-sm"
-            >
-            Increment
-            </button>
-        </React.Fragment>
-        );
-    }
+  getBadgeClasses() {
+    let classes = "badg m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    getBadgeClasses() {
-        let classes = "badg m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-
-formatCount() {
-    const { count } = this.state;
-    return count === 0? 'zero' : count;
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "zero" : value;
+  }
 }
-} 
- 
-export default Counter; 
+
+export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Counter from './counter'
+import Counter from './counter';
 
 
 class counters extends Component {
@@ -10,12 +10,34 @@ class counters extends Component {
             {id: 3, value: 0},
             {id: 4, value: 0}
         ]
+     }; 
+
+     handleReset = () => {
+        const counters = this.state.counters.map(c => {
+             c.value = 0;
+             return c;
+         });
+         this.setState({ counters });
+     }
+     
+     handleDelete = counterId => {
+       const counters = this.state.counters.filter(c => c.id !== counterId);
+       this.setState({counters}); 
      };
+
     render() { 
         return ( 
         <div>
-           {this.state.counters.map(counter =>
-             <Counter key={counter.id} value={counter.value} />)} 
+            <button 
+            onClick={this.handleReset}
+            className="btn btn-primary btn-sm m-2">Reset</button>
+           {this.state.counters.map(counter => (
+           <Counter 
+           key={counter.id} 
+           onDelete={this.handleDelete}
+           counter={counter}
+           />
+           ))} 
         </div> 
         );
     }
